cf271094e45f9bd1a973a83f81e547b5393acc08
Devika <devakumar.voleti@tcs.com>
2020-05-19 18:49:01 +0530

    initial commit

---
+++ src/main/java/com/ing/mvp/BankTransaction/BankMvpApplication.java
@@ -0,0 +1,13 @@
+package com.ing.mvp.BankTransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMvpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/mvp/BankTransaction/BankMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.mvp.BankTransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
